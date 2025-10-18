be6ac9331abdd125cbd2ce8ea1e2e182b5306746
Sobhan Tahmasebi <niinja2017@gmail.com>
2025-10-18 09:30:33 +0330

    Add lazy loading to Home and Login pages

--- src/AllSection.jsx
+++ src/AllSection.jsx
@@ -1,20 +1,28 @@
-import { useEffect, useState } from "react";
-import Home from "./Pages/Home/Home";
-import FormLogin from "./Pages/Login/FormLogin";
+import { lazy, Suspense, useEffect, useState } from "react";
+import LoadingItem from "./Loading/LoadingItem";
+
+const Home = lazy(() => import(/* webpackChunkName : 'Home-page' */ './Pages/Home/Home'))
+const FormLogin = lazy(() => import(/* webpackChunkName : 'From-Login' */'./Pages/Login/FormLogin'))
 
 export default function AllSection() {
 
-    const [changePage , setChangePage] = useState(JSON.parse(localStorage.getItem('changePage')) ?? false)
+    const [changePage, setChangePage] = useState(() =>
+        JSON.parse(localStorage.getItem('changePage')) ?? false
+    );
 
 
     useEffect(() => {
-        localStorage.setItem('changePage' , JSON.stringify(changePage))
-    } , [changePage])
+        localStorage.setItem('changePage', JSON.stringify(changePage))
+    }, [changePage])
 
     const changePageHandler = () => setChangePage(prev => !prev)
     return (
         <>
-            {changePage ? <FormLogin changePageHandler={changePageHandler}/> : <Home changePageHandler={changePageHandler}/>}
+            <Suspense fallback={<LoadingItem/>}>
+                {changePage
+                    ? <FormLogin changePageHandler={changePageHandler} />
+                    : <Home changePageHandler={changePageHandler} />}
+            </Suspense>
         </>
 
     )
---
+++ src/Loading/LoadingItem.jsx
@@ -0,0 +1,11 @@
+
+
+export default function LoadingItem() {
+    return (
+        <>
+            <div className="flex items-center justify-center h-screen">
+                <p className="text-white font-bold text-5xl border-3 rounded-2xl p-6">لطفا منتظر بمانید ...</p>
+            </div>
+        </>
+    )
+}
