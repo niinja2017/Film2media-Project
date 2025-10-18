715d0cfd6e5812e21f416957fc16c4a37fd1d5af
Sobhan Tahmasebi <niinja2017@gmail.com>
2025-10-18 13:04:23 +0330

    chore: remove AOS library and related animations

--- src/Components/Movie/MovieCard.jsx
+++ src/Components/Movie/MovieCard.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import ButtonCardMore from "../Ui/ButtonCardMore";
 
 import { Swiper } from "swiper/react";
@@ -7,21 +6,13 @@ import { Swiper } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 
-// Animate On Scroll
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
 
 
 export default function MovieCard({ title, children }) {
 
-    useEffect(() => {
-        AOS.init({ duration: 800, once: true , offset: 120 });
-    }, []);
-
     return (
         <>
-            <section data-aos='fade-left' data-aos-delay="300" className="w-[85%] mx-auto mt-[3.5rem] mb-[7.5rem] relative select-none">
+            <section className="w-[85%] mx-auto mt-[3.5rem] mb-[7.5rem] relative select-none">
                 <div>
                     <div className="flex justify-between">
                         <h3 className="text-white font-bold text-2xl">{title}</h3>
